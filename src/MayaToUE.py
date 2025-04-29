7f03f4d927b844e8b42c39d8032cfc3e1de55906
jili1@uiwtx.edu <jili1@uiwtx.edu>
2025-04-29 08:25:29 -0500

    send to unreal works

--- src/MayaToUE.py
+++ src/MayaToUE.py
@@ -5,6 +5,7 @@ from PySide2.QtGui import QIntValidator, QRegExpValidator
 from PySide2.QtWidgets import QCheckBox, QFileDialog, QHBoxLayout, QLabel, QLineEdit, QListWidget, QMessageBox, QPushButton, QVBoxLayout, QWidget
 import maya.cmds as mc
 import MayaPluginsSpring2025Sec1
+import remote_execution
 
 def TryAction(action):
     def wrapper(*args, **kwargs):
@@ -92,6 +93,11 @@ class MayaToUE:
         command = "".join(commands)
         print(command)
 
+        remoteExc = remote_execution.RemoteExecution()
+        remoteExc.start()
+        remoteExc.open_command_connection(remoteExc.remote_nodes)
+        remoteExc.run_command(command)
+        remoteExc.stop()
 
 
 
